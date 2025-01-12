e71d5ad9cbf38c1c585242bc42297717ac5498f9
Sudipto Islam <sudipto.islam2005@gmail.com>
2025-01-11 16:05:35 -0800

    fixing heroku 2

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+"""
 from quart import Quart
 from routes import routes_bp
 import os
@@ -13,3 +14,19 @@ app.register_blueprint(routes_bp)
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+"""
+
+    
+from quart import Quart
+
+# App setup
+app = Quart(__name__)
+
+@app.route("/")
+async def hello_world():
+    print("hello")
+    return "Hello world"
+
+# Start the app with Hypercorn
+if __name__ == "__main__":
+    app.run()
